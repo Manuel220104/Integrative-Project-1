065e3128ac0aefb5a8101b3dfa9a57fa4c159e31
manuel220104 <marangog3eafit.edu.co>
2023-08-23 18:16:21 -0500

    Cambio de lugar carpeta venv

--- frontend/src/components/AcentosCard.jsx
+++ frontend/src/components/AcentosCard.jsx
@@ -1,9 +1,9 @@
+import './AcentosCard.css'
 
 export function AcentosCard({Book}) {
     return (
         <div>
             <img src={Book.image}/>
-            <h1>{Book.image}</h1>
             <h1>{Book.title}</h1>
             <p>{Book.description}</p>
             <hr/>
