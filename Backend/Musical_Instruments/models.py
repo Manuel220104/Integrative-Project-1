657e1ce7f3f84f112d585ec6fa20f10f600429d8
manuel220104 <marangog3eafit.edu.co>
2023-08-28 22:58:57 -0500

    hamburguesa mejorada

--- Backend/Musical_Instruments/models.py
+++ Backend/Musical_Instruments/models.py
@@ -11,9 +11,9 @@ class Musical_Instrument(models.Model):
         ("Electrófonos", "Electrófonos"),
         ("Idiófonos", "Idiófonos")
     ]
-    ProductType = models.CharField(max_length = 20, choices=MUSICAL_INSTRUMENT_TYPE, default=False)
-    Brand = models.CharField(max_length =40, default=False)
-    Model = models.CharField(max_length =30, default=False)
+    ProductType = models.CharField(max_length = 20, choices=MUSICAL_INSTRUMENT_TYPE)
+    Brand = models.CharField(max_length =40)
+    Model = models.CharField(max_length =30)
     Product = models.OneToOneField(Product, on_delete=models.CASCADE)
     
     def __str__(self):
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -4,79 +4,56 @@ import search from '../assets/icons/search.png'
 import cart from '../assets/icons/cart.png'
 import login from '../assets/icons/login.png'
 import like from '../assets/icons/like.png'
-import styled from 'styled-components'
 import BurguerButton from './BurguerButton'
 
 export function Navigation(){
     return(
-        <>
-            <NavContainer>
-                <div class="navbar">
-                    
-                        <div class="navbar-items">
-                            <Link to="/">
-                                <img class="navbar-logo" src={Logo} alt="Logo"/>
-                            </Link>
-                            
-                            <div className="Links activate">
-                            <Link to="/">
-                                <div class="navbar-item"><a>Inicio</a></div>
-                            </Link>
-                            
-                            <Link to="/Productos">
-                                <div class="navbar-item"><a>Productos</a></div>
-                            </Link>
 
-                            <Link to="/Nosotros">
-                                <div class="navbar-item"><a>Acerca de nosotros</a></div>
-                            </Link>
-                            </div>
-                        </div>
-                
-                        <div class="navbar-search">
-                            <input class="navbar-item search-bar" type="text" placeholder=""/>
-                            <a href="" class="navbar-item search-icon" ><img  src={search} alt="Buscar"/></a>
-                            <img class="navbar-item cart-icon" src={cart} alt="Carrito"/>
-                            <Link to="/Mis-MeGusta">
-                            <img class="navbar-item like-icon" src={like} alt="Me gusta"/>
-                            </Link>
-                            
-                            <div className="Links activate">
-                                <div class="navbar-login">
-                                    <a class="login" href="">
-                                    <img class="login-icon" src={login} alt="login"/>
-                                    Iniciar sesión / Registrarse </a>
-                                </div>
-                            </div>
-                        </div>
+            <div class="navbar">
+                <div class="navbar-items">
+                    <Link to="/">
+                        <img class="navbar-logo" src={Logo} alt="Logo"/>
+                    </Link>
+
+
+                        <Link to="/">
+                            <div class="navbar-item ocultar"><a>Inicio</a></div>
+                        </Link>
+
+                        <Link to="/Productos">
+                            <div class="navbar-item ocultar"><a>Productos</a></div>
+                        </Link>
+                    
+                        <Link to="/Nosotros">
+                            <div class="navbar-item ocultar"><a>Acerca de nosotros</a></div>
+                        </Link>
+                    
                 </div>
 
-            <div className="burguer">
-            <BurguerButton/>
+                <div class="navbar-search">
+                    <input class="navbar-item search-bar ocultar" type="text" placeholder=""/>
+                    <a href="" class="navbar-item search-icon" ><img  src={search} alt="Buscar"/></a>
+                    <img class="navbar-item cart-icon" src={cart} alt="Carrito"/>
+                    <Link to="/Mis-MeGusta">
+                    <img class="navbar-item like-icon" src={like} alt="Me gusta"/>
+                    </Link>
+
+                    
+                        <div class="navbar-login">
+                            <a class="login" href="">
+                            <img class="login-icon ocultar" src={login} alt="login"/>
+                            <span class="ocultar"> Iniciar sesión / Registrarse</span>
+                            </a>
+                        </div>
+                    
+                        <div className="burguer">
+                    <BurguerButton/>
+                    </div>
+                    
+                </div>
+                
             </div>
-            </NavContainer>
-            
-        </>
-            
-        
-        
+
+
     )
 }
-
-
-const NavContainer = styled.nav`
-h2{
-    color: white;
-    font-weight: 400;
-    span{
-        font-weight:bold;
-    }
-}
-padding: .4rem;
-
-display: flex;
-align-items: center;
-justify-content: space-between;
-`
-
-
