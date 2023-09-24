4ea16cca1f9e1fec3725268078b0771118943ef8
manuel220104 <marangog3eafit.edu.co>
2023-09-24 15:47:04 -0500

    Prueba inicio de sesion

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ export function Login() {
                         src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                         alt="Workflow"
                     />
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Inicio de sesion</h2>
                     <p className="mt-2 text-center text-sm text-gray-600">
                         Or{' '}
                         <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
