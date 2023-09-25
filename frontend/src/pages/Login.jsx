14e2d1fe72aad7c13b8847c8134880aac9e94565
manuel220104 <marangog3eafit.edu.co>
2023-09-24 19:38:01 -0500

    cambio de base y front

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -56,17 +56,7 @@ export function Login() {
                             </div>
 
                             <div className="flex items-center justify-between">
-                                <div className="flex items-center">
-                                    <input
-                                        id="remember-me"
-                                        name="remember-me"
-                                        type="checkbox"
-                                        className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                    />
-                                    <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                        Remember me
-                                    </label>
-                                </div>
+                                
 
                                 <div className="text-sm">
                                     <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
