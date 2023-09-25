773a8ae3c3cb69955efd0d557417a5252e21ae5d
manuel220104 <marangog3eafit.edu.co>
2023-09-24 19:06:26 -0500

    Prueba #2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,18 +14,9 @@ export function Login() {
         */}
             <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="Workflow"
-                    />
+                    
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Inicio de sesion</h2>
-                    <p className="mt-2 text-center text-sm text-gray-600">
-                        Or{' '}
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                            start your 14-day free trial
-                        </a>
-                    </p>
+                    
                 </div>
 
                 <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
