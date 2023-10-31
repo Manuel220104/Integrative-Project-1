ba75bdff65bae6c30e229f93be3de46eb2a6c92d
manuel220104 <marangog3eafit.edu.co>
2023-10-31 10:41:32 -0500

    Se logra hacer el post de los likes, ahora sigue mostrar al usuario todos los productos a los cuales le ha dado like

--- frontend/src/api/Likes.api.js
+++ frontend/src/api/Likes.api.js
@@ -2,19 +2,16 @@ import axios from 'axios';
 
 const urlBack = "http://localhost:8000/";
 
-const API_BASE_URL = urlBack + "api/v1/";
 
 const LikesAPI = axios.create({
-    baseURL: API_BASE_URL + "Like/",
+    baseURL: urlBack + 'Likes/api/v1/', // Define la URL base personalizada
 });
 
-export const getAllLikes = () => LikesAPI.get('/');
 
-export const createLike = (product_id, username) => {
-    return LikesAPI.post('like_product/', {
-        product_id: product_id,
-        username: username,
-    });
-};
+export const getAllLikes = () => LikesAPI.get('/likes/');
+
+export const createLike = (likeData) => LikesAPI.post('like_product/', likeData);
+
 
 export const getLikeDetail = (like_id) => LikesAPI.get(`Likes/${like_id}/`);
+
--- frontend/src/components/NewsCarouselCard.jsx
+++ frontend/src/components/NewsCarouselCard.jsx
@@ -1,83 +1,93 @@
-    import React, { useState } from 'react';
-    import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import dislike from '../assets/icons/like.png';
+import like from '../assets/icons/likefull.png';
+import { createLike } from '../api/Likes.api';
+import cart from '../assets/icons/cart.png';
+import { getAllLikes } from '../api/Likes.api';
 
-    import like from '../assets/icons/like.png';
-    import likefull from '../assets/icons/likefull.png'; // Añade la imagen "likefull"
-    import { createLike } from '../api/Likes.api';
 
-    import cart from '../assets/icons/cart.png';
 
-    const MaxLength = 30;
-    function formatNumberWithCommas(input) {
-        if (typeof input !== 'number') {
-            input = parseFloat(input.replace(/,/g, ''));
-        }
-        return input.toLocaleString('es-ES');
+const MaxLength = 30;
+function formatNumberWithCommas(input) {
+    if (typeof input !== 'number') {
+        input = parseFloat(input.replace(/,/g, ''));
     }
+    return input.toLocaleString('es-ES');
+}
 
-    export function NewsCarouselCard({ Product }) {
-        const truncatedName =
-            Product.Name.length > MaxLength
-                ? `${Product.Name.substring(0, MaxLength)}...`
-                : Product.Name;
+export function NewsCarouselCard({ Product }) {
+    const truncatedName =
+        Product.Name.length > MaxLength
+            ? `${Product.Name.substring(0, MaxLength)}...`
+            : Product.Name;
 
-        // Iniciar el estado como true si la imagen inicial es "likefull", de lo contrario, false
-        const [liked, setLiked] = useState(Product.ImageUrl === likefull);
+    const [liked, setLiked] = useState(false); // Inicialmente, no se ha hecho "like"
 
-        const handleLikeClick = () => {
-            // Cambiar el estado en función de la imagen actual
-            setLiked((currentLiked) => !currentLiked);
-        
-            // Obtén el product_id de Product.ProductId y el username de la manera adecuada
-            const product_id = Product.ProductId; // Asegúrate de obtenerlo de la forma correcta
-            const username = "bb"; // Asegúrate de obtenerlo de la forma correcta
-        
-            // Crea el objeto de datos a enviar en el POST
-            const data = {
-                product_id: product_id,
-                username: username,
-            };
-        
-            createLike(data.product_id, data.username)
-                .then((response) => {
-                    if (response.status === 201) {
-                        // El "Me gusta" se ha registrado con éxito, puedes hacer algo aquí si lo deseas
-                    } else if (response.status === 204) {
-                        // El "Me gusta" se ha eliminado con éxito, puedes hacer algo aquí si lo deseas
-                    }
-                })
-                .catch((error) => {
-                    // Manejar errores de la API, puedes mostrar un mensaje de error si lo deseas
-                    console.error('Error al interactuar con la API:', error);
-                    // Imprime el error completo para depuración
-                    console.error(error);
-                });
+    useEffect(() => {
+        // Obtener el nombre de usuario o email del almacenamiento local
+        const username_or_email = localStorage.getItem('username_or_email');
+
+        // Realizar una solicitud para obtener los "likes" del usuario
+        getAllLikes()
+            .then((response) => {
+                if (response.status === 200) {
+                    // Verificar si el usuario ha dado "like" al producto actual
+                    const userLikes = response.data;
+                    const hasLiked = userLikes.some((dislike) => dislike.product_id === Product.ProductId);
+                    setLiked(hasLiked);
+                }
+            })
+            .catch((error) => {
+                console.error('Error al obtener los "likes":', error);
+            });
+    }, [Product.ProductId]); // Ejecutar cuando cambie el ID del producto
+    const handleLikeClick = () => {
+        setLiked((currentLiked) => !currentLiked);
+
+        const product_id = Product.ProductId;
+
+        const data = {
+            product_id: Product.ProductId,
+            username: "bb", // Puedes reemplazar "bb" con el valor del nombre de usuario que desees
         };
-        
+        console.log(data);
+        // Envía una solicitud POST a la API de Likes con el token de autenticación en los encabezados
+        createLike(data)
+            .then((response) => {
+                if (response.status === 201) {
+                    // "Me gusta" registrado con éxito
+                } else if (response.status === 204) {
+                    // "Me gusta" eliminado con éxito
+                }
+            })
+            .catch((error) => {
+                console.error('Error al interactuar con la API:', error);
+                console.error(error);
+            });
+    };
 
-        return (
-            <div className="cardNews">
-                <img className="imagencarrusel" src={Product.ImageUrl} alt={truncatedName} />
-                <div className="infoNews">
-                    <span className="TitleCardNews">{truncatedName}</span>
-                    <span className="PriceCard">$ {formatNumberWithCommas(Product.Price)}</span>
-                    <h2 className="tipoproducto">{Product.ProductType}</h2>
-                    <hr />
-                    <div className="iconsCard">
-                        
-                        <img
-                            className="navbar-item like-icon"
-                            src={liked ? likefull : like} // Cambia la imagen en función del estado "liked"
-                            alt="Me gusta"
-                            onClick={handleLikeClick} // Agregar un controlador de clic para cambiar el estado
-                        />
-                        <img className="navbar-item cart-icon" src={cart} alt="Carrito" />
-                    </div>
-                    <Link to={`/Productos/DetalleProducto/${Product.ProductId}`} state={{ Product }}>
-                        <span className="seeCard">Ver más</span>
-                    </Link>
+    return (
+        <div className="cardNews">
+            <img className="imagencarrusel" src={Product.ImageUrl} alt={truncatedName} />
+            <div className="infoNews">
+                <span className="TitleCardNews">{truncatedName}</span>
+                <span className="PriceCard">$ {formatNumberWithCommas(Product.Price)}</span>
+                <h2 className="tipoproducto">{Product.ProductType}</h2>
+                <hr />
+                <div className="iconsCard">
+                    <img
+                        className="navbar-item like-icon"
+                        src={liked ? like : dislike}
+                        alt="Me gusta"
+                        onClick={handleLikeClick}
+                    />
+                    <img className="navbar-item cart-icon" src={cart} alt="Carrito" />
                 </div>
+                <Link to={`/Productos/DetalleProducto/${Product.ProductId}`} state={{ Product }}>
+                    <span className="seeCard">Ver más</span>
+                </Link>
             </div>
-        );
-    }
-        
+        </div>
+    );
+}
